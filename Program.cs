92fe4fda279436501c4acf3faf93176c5a181276
dinucencio09 <133662891+dinucencio09@users.noreply.github.com>
2024-05-29 10:59:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+        /*1 Exercício*/
+    {
+        /*
+        int quantidade = 40;
+        double valorUnitario;
+
+        if (quantidade <= 25)
+            valorUnitario = 1.50;
+        else
+            valorUnitario = 1.25;
+
+        double total = quantidade * valorUnitario;
+        Console.WriteLine($"O cliente pagará R$ {total}.");
+        */
+
+
+
+        /*2 Exercício*/
+
+        /*
+        int numero = 70;
+        int resultado;
+
+        if (numero > 10)
+            resultado = numero + 10 * 2;
+        else
+            resultado = numero + 9 * 3 - 8;
+
+        Console.WriteLine($"O resultado é {resultado}.");
+        */
+
+
+
+        /*3 Exercício*/
+
+        /*
+        int numero = 10;
+
+        if (numero > 0)
+            Console.WriteLine("O número é positivo.");
+        else if (numero < 0)
+            Console.WriteLine("O número é negativo.");
+        else
+            Console.WriteLine("O número é zero.");
+        */
+
+
+
+        /*4 Exercício*/
+
+        /*
+        int num1 = 110;
+        int num2 = 40;
+
+        if (num1 > num2)
+            Console.WriteLine($"{num1} é maior que {num2}.");
+        else if (num2 > num1)
+            Console.WriteLine($"{num2} é maior que {num1}.");
+        else
+            Console.WriteLine("Os números são iguais.");
+        */
+
+
+
+        /*5 Exercício*/
+
+        /*
+        double nota1 = 8.5;
+        double nota2 = 1.0;
+
+        double media = (nota1 + nota2) / 2;
+
+        if (media >= 7)
+            Console.WriteLine("Aprovado");
+        else
+            Console.WriteLine("Reprovado");
+        */
+    }
+}
